4bfd9b6bdae190a4dd7db967c712a9b43d4c67c7
John Doe <mail.space.git@proton.me>
2024-03-17 18:30:38 +0300

    Unique movie name FIX

--- models/movie.go
+++ models/movie.go
@@ -7,7 +7,7 @@ import (
 type Movie struct {
 	gorm.Model
 	ID          uint   `json:"ID" gorm:"primaryKey"`
-	Name        string `json:"name" gorm:"not null; size:150"`
+	Name        string `json:"name" gorm:"not null; size:150; unique"`
 	Description string `json:"description" gorm:"not null; size:1000"`
 	Date        string `json:"date" gorm:"not null"`
 	Rating      int    `json:"rating" gorm:"not null"` // TODO валидировать рейтинг от 1 до 10
